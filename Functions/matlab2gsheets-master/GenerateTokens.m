32f718c2bc236424e9e2270fdf700048b6bf09c5
Wouter Muijres <wouter.muijres@kuleuven.be>
2021-11-12 13:57:28 +0100

    Add function to write to google spread sheets

---
+++ Functions/matlab2gsheets-master/GenerateTokens.m
@@ -0,0 +1,5 @@
+clear, close all
+
+ClientID = '780589220651-qfcbo5sq7vdchqp0jph33esauta0nrbl.apps.googleusercontent.com';
+ClientSecret = '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';
+RunOnce(ClientID,ClientSecret)
